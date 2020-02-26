a0dd0ca0a97e19945a417add518096c69d99016a
huhn511 <sehe89@gmail.com>
2020-02-26 17:59:19 +0100

    fix particle js

--- frontend/src/components/ParticlesJS.vue
+++ frontend/src/components/ParticlesJS.vue
@@ -135,9 +135,12 @@ export default {
 
 <style>
 #particles-js {
-    position:absolute;
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
-    /* min-height: 100vh; */
+    min-height: 100vh;
+    height: 100vh;
     z-index: -1;
 }
 </style>
